b18a0c27aede3cdd2c22970d60822e5c0ba4c8ee
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-09-25 21:39:31 +0200

    added new public and views folders

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+        res.send('Settings Bill App') 
+});
+
+app.get('/', function(req, res) {
+    res.send('Settings Bill App') 
+});
+
+app.get('/settings', function(req, res) {
+    res.send('') 
+});
+
+app.get('/action', function(req, res) {
+    res.send('') 
+});
+
+app.get('/actions', function(req, res) {
+    res.send('') 
+});
+
+app.get('/actions/:type', function(req, res) {
+    res.send('') 
+});
+
+const PORT = process.env.PORT || 3011;
+
+app.listen(PORT, function(){
+    console.log("App started at port:", PORT)
+});
