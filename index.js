09c4b55937423ebbbec221f52254c484b5032fb6
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-10-03 21:01:42 +0200

    added main-handlebars

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require('express');
-
+const exphbs  = require('express-handlebars');
 const app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-        res.send('Settings Bill App') 
+        res.render('index') 
 });
 
 app.post('/settings', function(req, res) {
-    res.send('') 
+    
 });
 
 app.post('/action', function(req, res) {
-    res.send('') 
+
 });
 
 app.get('/actions', function(req, res) {
-    res.send('') 
+    
 });
 
 app.get('/actions/:type', function(req, res) {
-    res.send('') 
+    
 });
 
 const PORT = process.env.PORT || 3011;
