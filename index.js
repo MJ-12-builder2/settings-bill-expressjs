85c2e5e53fa05c87979a63a948bffdcdf20ef7a5
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-10-14 22:35:43 +0200

    added a new factory function

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const app = express();
+const bodyParser = require('body-parser');
+const settingsBill = require('./settings-bill');
+
+
+const app = express ();
+const settingsBill = settingsBill();
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json())
+
+
+
 app.get('/', function(req, res) {
-        res.render('index') 
-});
+        res.render('index', {
+            settings: settingsBill.getSettings()});
+    }),
+
 
 app.post('/settings', function(req, res) {
-    
+    settingsBill.setSettings({
+    callCost: req.body.callCost,
+    smsCost: req.body.smsCost,
+    warningLevel: req.body.warningLevel,
+    criticalLevel: req.body.criticalLevel,
+})
+
+        console.log(settingsBill.getSettings());
+
+        res.redirect('/');
 });
+
 
 app.post('/action', function(req, res) {
 
@@ -27,7 +53,16 @@ app.get('/actions/:type', function(req, res) {
     
 });
 
+
+
+
+
+
+
+
 const PORT = process.env.PORT || 3011;
+
+app.use(express.static('public'));
 
 app.listen(PORT, function(){
     console.log("App started at port:", PORT)
