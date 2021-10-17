f2c72e71e184387ba3f92d13c8e301386d06e94f
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-10-17 04:49:05 +0200

    amended settings bill expression

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
-const settingsBill = require('./settings-bill');
+const SettingsBill = require('./settings-bill');
 
 
 const app = express ();
-const settingsBill = settingsBill();
+const settingsBill = SettingsBill();
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -15,31 +15,27 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // parse application/json
 app.use(bodyParser.json())
 
-
-
 app.get('/', function(req, res) {
         res.render('index', {
-            settings: settingsBill.getSettings()});
+            settings: settingsBill.getSettings()
+        });
     }),
 
+app.post('/settings', function(req, res) {  
+   settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel,
+    })
 
-app.post('/settings', function(req, res) {
-    settingsBill.setSettings({
-    callCost: req.body.callCost,
-    smsCost: req.body.smsCost,
-    warningLevel: req.body.warningLevel,
-    criticalLevel: req.body.criticalLevel,
-})
-
-        console.log(settingsBill.getSettings());
-
-        res.redirect('/');
+    res.redirect('/');
 });
-
 
 app.post('/action', function(req, res) {
 
@@ -53,13 +49,6 @@ app.get('/actions/:type', function(req, res) {
     
 });
 
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3011;
 
 app.use(express.static('public'));
@@ -67,3 +56,5 @@ app.use(express.static('public'));
 app.listen(PORT, function(){
     console.log("App started at port:", PORT)
 });
+
+
